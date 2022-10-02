2c7256eb3e7540a67249d589aaf146f30fec162a
tjlinch <tjlinch@gmail.com>
2022-10-02 14:26:58 -0600

    created routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+const{
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// api/thoughts/:id
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController');
+
+// /api/users: get and post for all users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:id selects user by id to get 1 user, edit 1 user, or delete 1 user
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/user/:id/friends/:friendId for friends post and delete
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;

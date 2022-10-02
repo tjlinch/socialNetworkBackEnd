3522ed514feae786ecfd68aa1ab22b15bbd2ff8d
tjlinch <tjlinch@gmail.com>
2022-10-02 15:48:54 -0600

    controller setup

---
+++ controllers/thoughtController.js
@@ -0,0 +1,32 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    
+    getThoughts(req, res) {
+
+    },
+    
+    getThoughtById(req, res) {
+
+    },
+    
+    createThought(req, res) {
+
+    },
+    
+    updateThought(req, res) {
+
+    },
+    
+    deleteThought(req, res) {
+
+    },
+    
+    createReaction(req, res) {
+
+    },
+    
+    deleteReaction(req, res) {
+
+    },
+};
---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const { User } = require('../models');
+
+module.exports = {
+    
+    getUsers(req, res) {
+
+    },
+    
+    getUserById(req, res) {
+
+    },
+    
+    createUser(req, res) {
+
+    },
+    
+    updateUser(req, res) {
+
+    },
+    
+    deleteUser(req, res) {
+
+    },
+    
+    addFriend(req, res) {
+
+    },
+    
+    removeFriend(req, res) {
+
+    },
+};

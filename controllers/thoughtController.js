3e3f19237b0faa6c6d87110dfe5f23fc22141c98
tjlinch <tjlinch@gmail.com>
2022-10-02 22:52:03 -0600

    thought controller in progress

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,17 +1,40 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 module.exports = {
-    
+    // get all thoughts
     getThoughts(req, res) {
-
+        Thought.find()
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
     },
-    
+    // get a single thought
     getThoughtById(req, res) {
-
+        Thought.findOne({ _id: req.params.thoughtId })
+            .populate('reactions')
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thoughts with this ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
     },
-    
+    // create a new thought
     createThought(req, res) {
-
+        Thought.create(req.body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $push: { thoughts: _id }},
+                    { new: true }
+                );
+            })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No user matching this ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     
     updateThought(req, res) {
